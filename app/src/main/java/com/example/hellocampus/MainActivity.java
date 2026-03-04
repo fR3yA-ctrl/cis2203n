dbdf76eb3bd074e83dff8f661a9de479e45a4cb0
Freya Hermosilla <freyalouise.hermosilla@gmail.com>
2026-03-04 19:16:18 +0800

    Part 1 Submission - HelloCampus with Screenshot

---
+++ app/src/main/java/com/example/hellocampus/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.hellocampus;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(getString(R.string.student_id));
+        }
+        // Get views
+        Button myButton = findViewById(R.id.btnChangeName);
+        TextView myText = findViewById(R.id.tvDepartment);
+
+        // Button click
+        myButton.setOnClickListener(v -> {
+            myText.setText(R.string.chairwoman); // replace with real chairwoman
+        });
+
+
+    }
+}
