69637bb64c9593a6e564f1a386a6afed85ce0dd5
Freya Hermosilla <freyalouise.hermosilla@gmail.com>
2026-03-04 19:26:08 +0800

    Part 2 - Interrupted Counter fixed with state preservation

--- app/src/main/java/com/example/hellocampus/MainActivity.java
+++ app/src/main/java/com/example/hellocampus/MainActivity.java
@@ -1,40 +1,56 @@
 package com.example.hellocampus;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCounter = 0;
+    private TextView counterDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(getString(R.string.student_id));
-        }
-        // Get views
-        Button myButton = findViewById(R.id.btnChangeName);
-        TextView myText = findViewById(R.id.tvDepartment);
+        // FIXED SABOTAGE
+        counterDisplay = findViewById(R.id.tvCounter);
+        counterDisplay.setText("0");
 
-        // Button click
-        myButton.setOnClickListener(v -> {
-            myText.setText(R.string.chairwoman); // replace with real chairwoman
+        Button btnIncrement = findViewById(R.id.btnIncrement);
+
+        btnIncrement.setOnClickListener(v -> {
+            mCounter++;
+            counterDisplay.setText(String.valueOf(mCounter));
         });
+    }
 
+    // SAVE counter before rotation destroys activity
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("COUNT_KEY", mCounter);
+    }
 
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mCounter = savedInstanceState.getInt("COUNT_KEY");
+        counterDisplay.setText(String.valueOf(mCounter));
     }
 }
